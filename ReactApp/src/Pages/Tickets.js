5145aed8f0b3ee4e295bad078722bd6a83f7a793
Hugo Herter <git@hugoherter.com>
2016-07-03 23:30:31 +0200

    Add description for Tickets page

--- ReactApp/src/Pages/Tickets.js
+++ ReactApp/src/Pages/Tickets.js
@@ -4,13 +4,29 @@ import CustomTitle from '../Cards/CustomTitle';
 import PresalesCard from '../Cards/Tickets/PresalesCard';
 import OnSiteCard from '../Cards/Tickets/OnSiteCard';
 
+const i18n_strings = {
+  fr: {
+    title: 'Tickets',
+    desc: 'Sur place et Préventes',
+  },
+  en: {
+    title: 'Tickets',
+    desc: 'On site and pre sales',
+  },
+  nl: {
+    title: 'Tickets',
+    desc: 'Ter plaatse / Voorverkoop',
+  },
+}
 
 export default class TicketsPage extends React.Component {
 
   render() {
+    var strings = i18n_strings[this.props.lang] || i18n_strings['fr'];
+
     return (
       <div>
-        <CustomTitle title="Tickets" />
+        <CustomTitle title={strings.title} desc={strings.desc} />
         <PresalesCard lang={this.props.lang} />
         <OnSiteCard lang={this.props.lang} />
       </div>
