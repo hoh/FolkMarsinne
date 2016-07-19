56614f36954739083cb4eca59e2f9a6c4d4101d1
Jeremvan <jeremvan@gmail.com>
2016-07-19 22:50:32 +0200

    Update FridayProgram.js

--- ReactApp/src/Cards/Program/FridayProgram.js
+++ ReactApp/src/Cards/Program/FridayProgram.js
@@ -6,20 +6,20 @@ import {DayProgram} from './ProgramComponents'
 const fridayPlan = [
   {hour: '', 'groups': []},
   {hour: '20h', 'groups': [false, 
-  			   {name: 'Baila Tocamos (BE) <i>bal</i>', duration: 4, kind: 'bal'},
+  			   {name: 'Baila Tocamos (BE) bal', duration: 4, kind: 'bal'},
   			   false, false]},
   {hour: '20h30', 'groups': [false, true, false, false]},
   {hour: '21h', 'groups': [false, true, false, false]},
   {hour: '21h30', 'groups': [false, true, false, false]},
-  {hour: '22h', 'groups': [{name: 'Duo Montanaro-Cavez (BE-FR) <i>concert</i>', duration: 3, kind: 'concert'},
+  {hour: '22h', 'groups': [{name: 'Duo Montanaro-Cavez (BE-FR) concert', duration: 3, kind: 'concert'},
                            false,
-                           {name: 'Wouter Vandenabeele et Jeroen Knapen (BE) <i>bal</i>', duration: 4, kind: 'bal'},
+                           {name: 'Wouter Vandenabeele et Jeroen Knapen (BE) bal', duration: 4, kind: 'bal'},
                            false]},
   {hour: '22h30', 'groups': [true, false, true, false]},                         
   {hour: '23h', 'groups': [true, false, true, false]},
   {hour: '23h30', 'groups': [false, false, true, false]},
   {hour: '00h', 'groups': [false,
-                           {name: 'Les Zéoles (FR) <i>bal</i>', duration: 4, kind: 'bal'},
+                           {name: 'Les Zéoles (FR) bal', duration: 4, kind: 'bal'},
                            false, false]},
   {hour: '00h30', 'groups': [false, true, false, false]},
   {hour: '01h', 'groups': [false, true, false, false]},
@@ -30,7 +30,7 @@ const fridayPlan = [
 
 export default class FridayProgram extends React.Component {
   render() {
-		return (
+    return (
       <DayProgram plan={fridayPlan} lang={this.props.lang} />
     );
   }
